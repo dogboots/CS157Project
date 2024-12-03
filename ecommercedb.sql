51a433f9f810fc679bfd8dfcf5c317cad555714e
Leo Truong <104650216+Leo-Truong@users.noreply.github.com>
2024-12-03 03:07:25 -0800

    Added more dummy data

--- ecommercedb.sql
+++ ecommercedb.sql
@@ -110,26 +110,113 @@ CREATE TABLE Review (
 INSERT INTO Address (StreetName, City, State, ZipCode) VALUES
 ('1 Washington Sq', 'San Jose', 'CA', 95192),
 ('123 Test St', 'San Jose', 'CA', 95192),
-('456 Random Dr', 'Los Angeles', 'CA', 90001)
+('456 Random Dr', 'Los Angeles', 'CA', 90001),
+('456 Test St', 'Los Angeles', 'CA', 90001),
+('123 Fashion St', 'San Jose', 'CA', 95192)
 ;
 
 INSERT INTO User (Username, Password, Email, Role, AddressID) VALUES
 ('user_buyer', 'buy123', 'buyer@gmail.com', 'User', 1),
 ('admin1', 'password456', 'admin@hotmail.com', 'Admin', 2),
-('user_seller', 'sell123', 'seller@yahoo.com', 'User', 3)
+('user_seller', 'sell123', 'seller@yahoo.com', 'User', 3),
+('isellthings', 'selling123', 'isellthings@gmail.com', 'User', 4),
+('fashionicon', 'fashion123', 'fashionicon@hotmail.com', 'User', 5)
 ;
 
 INSERT INTO Category (CategoryName) VALUES
 ('Electronics'),
 ('Books'),
-('Clothing'),
-('Home & Kitchen')
+('Fashion'),
+('Home & Kitchen'),
+('Toys'),
+('Sports')
 ;
 
 INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
 (3, 1, 'Laptop', 'General use laptop with high performance', 699.99, 50, '/images/laptop.jpg', '2023-01-01'),
 (3, 2, 'Dictionary', 'A regular dictionary', 19.99, 100, '/images/dictionary.jpg', '2022-11-15'),
 (3, 3, 'T-shirt', 'A simple t-shirt', 14.99, 200, '/images/tshirt.jpg', '2022-08-20')
+;
+
+-- Dummy data for electronics 
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(4, 1, 'Smartphone', '6.5-inch display, 64GB storage', 499.99, 100, '/images/smartphone.jpg', '2023-09-01'),
+(4, 1, 'Smartwatch', 'Health tracking, GPS', 149.99, 150, '/images/smartwatch.jpg', '2023-10-10'),
+(4, 1, 'Bluetooth Speaker', 'Compact, 12-hour battery', 49.99, 300, '/images/bluetoothspeaker.jpg', '2023-08-20'),
+(4, 1, 'Camera', 'Waterproof, 1080p recording', 199.99, 80, '/images/bluetoothspeaker.jpg', '2023-09-15'),
+(4, 1, 'Wireless Earbuds', 'Noise-cancelling, 10-hour life', 99.99, 200, '/images/wirelessearbuds.jpg', '2023-11-15'),
+(3, 1, 'Desk Fan', 'Adjustable speed, quiet', 29.99, 120, '/images/deskfan.jpg', '2023-06-10'),
+(3, 1, 'LED Lamp', 'Adjustable brightness', 19.99, 250, '/images/ledlamp.jpg', '2023-07-05'),
+(3, 1, 'TV', 'Classic TV for everyday use', 399.99, 120, '/images/tv.jpg', '2023-10-15'),
+(3, 1, 'Mouse', 'Wireless mouse with adjustable DPI', 29.99, 200, '/images/mouse.jpg', '2022-09-15')
+;
+
+-- Dummy data for books
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(3, 2, 'Thesaurus', 'A classic thesaurus for finding synonyms and antonyms.', 19.99, 150, '/images/thesaurus.jpg', '2021-07-05'),
+(3, 2, 'The Very Hungry Caterpillar', 'A classic childrens book', 11.99, 50, '/images/caterpillarbook.jpg', '2022-10-10'),
+(4, 2, 'The Giving Tree', 'Shel Silverstein: The Giving Tree', 10.99, 300, '/images/givingtree.jpg', '2021-08-20'),
+(4, 2, 'The Lightning Thief', 'First Percy Jackson book', 20.99, 80, '/images/lightningthief.jpg', '2009-09-15'),
+(4, 2, 'The Sea of Monsters', 'Second Percy Jackson book', 20.99, 90, '/images/seaofmonsters.jpg', '2011-11-15'),
+(4, 2, 'Moby-Dick', 'A book about the whale', 29.99, 50, '/images/mobydick.jpg', '2018-06-10'),
+(4, 2, 'Wuthering Heights', 'Classic novel with romance and revenge', 19.99, 250, '/images/wutheringheights.jpg', '2022-07-05'),
+(4, 2, 'The Lord of the Rings', 'Classic fantasy novel', 19.99, 120, '/images/lotr.jpg', '2021-10-15'),
+(4, 2, 'Frankenstein', 'story about a science experiment gone wrong', 15.99, 200, '/images/frankenstein.jpg', '2022-09-15')
+;
+
+-- Dummy data for fashion
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(5, 3, 'Jeans', 'Very new jeans', 19.99, 150, '/images/jeans.jpg', '2021-08-05'),
+(5, 3, 'Sweater', 'Warm sweater', 25.99, 50, '/images/sweater.jpg', '2022-11-10'),
+(5, 3, 'Socks', 'Comfy pair of socks', 8.99, 300, '/images/socks.jpg', '2021-08-20'),
+(5, 3, 'Glasses', 'Fashionable glasses', 149.99, 20, '/images/glasses.jpg', '2021-09-15'),
+(5, 3, 'Gloves', 'Fashionable gloves', 200.99, 25, '/images/gloves.jpg', '2020-11-15'),
+(5, 3, 'Shoes', 'Good Shoes, Last a lifetime', 19.99, 50, '/images/shoes.jpg', '2018-06-10'),
+(5, 3, 'Necklace', 'Very cute necklace', 25.99, 250, '/images/necklace.jpg', '2022-07-05'),
+(5, 3, 'Bracelet', 'Very cute braclet', 39.99, 120, '/images/bracelet.jpg', '2021-10-15'),
+(5, 3, 'Ring', 'Very pretty ring', 399.99, 25, '/images/ring.jpg', '2022-09-15')
+;
+
+-- Dummy data for Home & Kitchen
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(4, 4, 'Fork', 'Perfect for eating pasta', 4.99, 150, '/images/fork.jpg', '2021-08-05'),
+(4, 4, 'Spoon', 'Perfect for some soup', 4.99, 50, '/images/spoon.jpg', '2022-11-10'),
+(4, 4, 'Chopstick', 'Asian eating utensil', 4.99, 300, '/images/chopsticks.jpg', '2021-08-20'),
+(4, 4, 'Cup', 'Useful for any kind of drink', 7.99, 20, '/images/cup.jpg', '2021-09-15'),
+(4, 4, 'Plate', 'Necessary for daily meals', 10.99, 25, '/images/plate.jpg', '2020-11-15'),
+(4, 4, 'Pan', 'Perfect to sear the perfect steak', 19.99, 50, '/images/pan.jpg', '2018-06-10'),
+(4, 4, 'Bottle', 'Useful for water or any drink', 15.99, 250, '/images/bottle.jpg', '2022-07-05'),
+(4, 4, 'Spatula', 'Perfect for morning pancakes', 15.99, 120, '/images/spatula.jpg', '2021-10-15'),
+(4, 4, 'Bowl', 'Perfect for some soup', 10.99, 25, '/images/bowl.jpg', '2022-09-15'),
+(4, 4, 'Toaster', 'Perfect for making some toast', 50.99, 25, '/images/toaster.jpg', '2022-09-15')
+;
+
+-- Dummy data for Toys
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(4, 5, 'Toy Car', 'Friction Powered Cars Toys for Toddlers', 4.99, 150, '/images/toycar.jpg', '2021-08-05'),
+(4, 5, 'Teddy Bear', 'Stuffed toy in the form of a bear', 13.99, 50, '/images/teddybear.jpg', '2022-11-10'),
+(4, 5, 'Dinosaur Toy', 'Fierce dinosaur toy for kids who crave adventure', 13.99, 300, '/images/dinosaurtoy.jpg', '2021-08-20'),
+(4, 5, 'Bakugan', 'Special Attack Dragonoid', 30.99, 20, '/images/bakugan.jpg', '2021-09-15'),
+(4, 5, 'Beyblade', 'Popular toy for children', 39.99, 25, '/images/beyblade.jpg', '2020-11-15'),
+(4, 5, 'Doll', 'Cute toy for children', 15.99, 50, '/images/doll.jpg', '2018-06-10'),
+(4, 5, 'Toy Piano', 'Toy Piano to keep your child happy', 15.99, 250, '/images/toypiano.jpg', '2022-07-05'),
+(4, 5, 'Toy Phone', 'Perfect to keep children busy', 15.99, 120, '/images/toyphone.jpg', '2021-10-15'),
+(3, 5, 'Toy Train', 'Very fun toys for toddlers', 10.99, 25, '/images/toytrain.jpg', '2022-09-15'),
+(3, 5, 'Action figure', 'Cool toy for kids who love adventure', 50.99, 25, '/images/actionfigure.jpg', '2022-09-15')
+;
+
+-- Dummy data for Sports
+INSERT INTO Product (SellerID, CategoryID, ProductName, ProductDescription, ProductPrice, StockQuantity, ProductImage, ReleaseDate) VALUES
+(4, 6, 'Basketball', 'The best long-lasting basketball', 4.99, 150, '/images/basketball.jpg', '2021-08-05'),
+(4, 6, 'Soccerball', 'Perfect to kick around on the field', 8.99, 50, '/images/soccerball.jpg', '2022-11-10'),
+(4, 6, 'Baseball bat', 'Easy to swing, made with good material', 13.99, 300, '/images/baseballbat.jpg', '2021-08-20'),
+(4, 6, 'Golf Ball', 'Lowest prices of the season', 3.99, 20, '/images/golfball.jpg', '2021-09-15'),
+(4, 6, 'Tennis Racquet', 'Best selection of quality tennis racquets', 19.99, 25, '/images/tennisracquet.jpg', '2020-11-15'),
+(3, 6, 'Tennis Ball', 'High quality, long-lasting', 9.99, 50, '/images/tennisball.jpg', '2018-06-10'),
+(3, 6, 'Volleyball', 'Simple design, water resistant', 35.99, 250, '/images/volleyball.jpg', '2022-07-05'),
+(3, 6, 'Baseball', 'The ball used in the sport of baseball', 15.99, 120, '/images/baseball.jpg', '2021-10-15'),
+(3, 6, 'Baseball gloves', 'Comfortable, easy to use', 10.99, 25, '/images/baseballgloves.jpg', '2022-09-15'),
+(3, 6, 'Football', 'Designed to meet the needs of players at every level', 36.99, 25, '/images/football.jpg', '2022-09-15')
 ;
 
 INSERT INTO Inventory (SellerID, ProductID, Quantity) VALUES
